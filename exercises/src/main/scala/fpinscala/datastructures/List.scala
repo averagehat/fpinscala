ceaea6aa39a90affaaa82295903863e05283420f
michaelpanciera <michael.panciera.work@gmail.com>
2016-01-22 19:36:46 -0500

    completed List exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -64,5 +64,75 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+      case Cons(x, xs) => Cons(f(x), map(xs)(f))
+      case _ => Nil }
+
+def append[A](a1: List[A], a2: List[A]): List[A] =
+  foldRight(a1,a2)((x,xs) => Cons(x,xs))
+
+// can sort of figure this out just from knowing
+// the type of append
+//foldRight accumulates arguments differently so it's faster to use
+def flatten[A](l: List[List[A]]): List[A] = 
+    List.foldRight(l, Nil:List[A])(List.append)
+
+def addOneToAll(l: List[Int]): List[Int] = map(l)(_+1)
+//foldRight then z appears at the end
+def showDoubles(doubles: List[Double]) = foldLeft(doubles, "")(_.toString + ", " + _.toString).tail.tail
+
+def filter[A](l: List[A])( f: (A => Boolean)): List[A] = l match {
+    case Cons(x, xs) if f(x) =>  Cons(x, filter(xs)(f))
+    case Cons(x, xs) => filter(xs)(f)
+    case _ => Nil 
 }
+
+filter(List())(_ % 2 == 0)
+val flatMap = (map _) andThen 
+def flatMap[A,B](l: List[A])(f: (A => List[B])): List[B] = flatten(map(l)(f))
+
+
+def flatMapFilter[A](l: List[A])( f: (A => Boolean)): List[A] = 
+    flatMap(l)(x => if (f(x)) List(x) else Nil)
+
+
+def addEach(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+    case (Cons(x, xs), Cons(y,ys)) => Cons(x + y, addEach(xs, ys))
+    case _ => Nil
+}
+
+  
+def zipWith[A,B,C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = (l1, l2) match {
+    case (Cons(x, xs), Cons(y,ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+    case _ => Nil
+}
+def exists[A](l: List[A])(f: A=>Boolean): Boolean = l match {
+    case cons(x, xs) if f(x) => true
+    case cons(x, xs) => exists(xs)(f)
+    case _ => false 
+}
+
+val contains = (xs, e) => exists(xs)(_ == e)
+
+def contains[A](l: List[A], e: A): Boolean = exists(l)(_ == e)
+def head[A](l: List[A]): A = l match { 
+   case Cons(x, _) => x
+   case _ => throw new Exception() }
+
+def tail[A](l: List[A]) = l match { 
+   case Cons(_, xs) => xs
+   case _ => throw new Exception() }
+
+def tails[A](l: List[A]): List[List[A]] = l match {
+    case Cons(x, xs) => Cons(xs, tails(xs))
+    case _ => Nil }
+
+def length[A](l: List[A]): Int = List.foldRight(l, 0)((_,acc) => acc + 1)
+def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = 
+    contains(map(tails(sup))((xs) => length(zipWith(xs, sub)(_ == _))), length(sub))
+
+
+
+def hasSubsequence[A](sup: Seq[A], sub: Seq[A]): Boolean =
+    val tails = sup.tails.filter(_.length >= sub.length)
+    tails.map(xs => (xs, sub).zipped.forall(_ == _)).contains(true)
